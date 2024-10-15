ca6e1bf6bf1cc3a8efd71e73236f08c23e3219d6
203051-chomiak <magda.chomiak2000@gmail.com>
2024-10-15 12:31:42 +0200

    Add files via upload
    Wykonanie zadania 3

---
+++ 2-IntroToR2/Zadanie3.R
@@ -0,0 +1,19 @@
+---
+title: "Zadanie 3"
+output: html_document
+date: "2024-10-15"
+---
+
+# Wprowadzenie
+
+Raport z zajęć analizy bioinformatyczne w genomice obejmujący naukę pobierania i używania pakietów.
+
+# Opis środowiska
+
+Zainstalowano pakiet: **ggplot2**, który wykorzystywany jest do wizualizacji danych w systemie R oraz pakiet **acdcR**, służący do obliczania zmiennych w produkcji rolnej lub analizzy rolniczo-klimatycznej.
+
+*Niestety nie umiem wykorzystać wybranego przeze mnie pakietu acdcR.*
+
+# Przykładowy kod
+
+print("Jak wykorzystać ten pakiet? ~~POMOCY!~~")
