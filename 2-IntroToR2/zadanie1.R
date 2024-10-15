5529fc3b33800c8975f87468df6404d75ea0a40f
203051-chomiak <magda.chomiak2000@gmail.com>
2024-10-15 10:59:42 +0200

    Add files via upload
    Rozwiązanie zadania 1

---
+++ 2-IntroToR2/zadanie1.R
@@ -0,0 +1,19 @@
+
+#------------------FUNKCJE POSZCZEGÓLNYCH PANELI-----------------
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+  
+  # Source (edytor skryptów) służy do pisania skryptów, a funkcje nie są automatycznie wykonywane 
+  # Console służy do wyświetlania funcki uruchomionych w edytorze; służy do szybkiego pobierania pakietów
+  # Enviroment (środowisko) służy do pokazywania wszystkiego, co sobie przypisaliśmy
+  # Output (panel wyjściowy) importowanie danych, tworzenie plików; kółko zębate - "set as working directory" - gdzie jest ścieżka którą chcemy napisać
+  # miotełka w konsoli czyści wizualnie zapiski 
+#-----------------MOTYW KOLORYSTYCZNY---------------
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+#wybrałam motyw Pastel on Dark, ponieważ lubię ciemne motywy i kolory nie są tak jaskrawe jak w innych motywach
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
